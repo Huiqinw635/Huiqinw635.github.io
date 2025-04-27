16cc90d0c9f4f1c0a283dcc22db653f5530c73e6
Huiqinw635 <hwang635@asu.edu>
2025-04-26 21:14:35 -0400

    Week6

---
+++ simpleJS.js
@@ -0,0 +1,10 @@
+//define some variables
+
+let numExample = 50
+var stringExample = "I'm a string"
+const pi = 3.14159
+
+console.log('The variable numExample has the value of ${numExample}')
+
+console.log(typeof stringExample)
+
